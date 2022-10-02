368229e1210eab61d8d7b157852975f051140a6f
vishal-Tx <vishalguptaa2z111@gmail.com>
2022-10-02 19:42:49 +0530

    minor syntax change.

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,15 +2,15 @@ import React from "react";
 import "./Note.css";
 import DeleteIcon from '@mui/icons-material/Delete';
 
-function Note(props) {
+function Note({onDelete, id, title, content}) {
   function handleClick() {
-    props.onDelete(props.id);
+    onDelete(id);
   }
 
   return (
     <div className="note">
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
+      <h1>{title}</h1>
+      <p>{content}</p>
       <button onClick={handleClick}><DeleteIcon /></button>
     </div>
   );
